5f0fc5a387b87ef1e6cafa17401c2f692f365ed7
cleudys <cleudys92@gmail.com>
2020-11-06 22:15:42 -0400

    He quitado un codigo innecesario para el proyecto.

--- Git.R
+++ Git.R
@@ -1,2 +1,21 @@
 print("Este archivo fue creado dentro de Rstudio")
 print("y ahora este archivo vive en Github")
+
+
+install.packages("corrplot")
+library(corrplot)
+
+corrplot(datos,method ="number",type = "upper")
+install.packages("GGally")
+install.packages("Hmisc")
+install.packages("PerformanceAnalytics")
+
+library(GGally)
+library(Hmisc)
+library(PerformanceAnalytics)
+
+matriz<-rcorr(as.matrix(datos))
+View(datos)
+datos<-mtcars
+
+round(cor(datos),2)
